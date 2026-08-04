8a2c0d5a5a9291934393fec2cb80beaa8167c50f
moririn@60 <okoume.daisuki@gmail.com>
2026-08-04 15:23:18 +0900

    Header.tsx内のタグ修正

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,12 @@
 import logo from '../assets/easy-debate-logo.png';
+import Heading from '../components/Heading';
 
 function Header() {
   return (
     <header className="bg-white border-b border-[#e4e2dd]">
       <div className="mx-auto h-16 flex flex-raw items-center px-5">
         <img className="flex w-10 h-10" src={logo} alt="easy-debate-app-logo" />
-        <h1 className="flex ml-3">やさしいディベート</h1>
+        <Heading level={2} className="flex ml-3">やさしいディベート</Heading>
       </div>
     </header>
   );
