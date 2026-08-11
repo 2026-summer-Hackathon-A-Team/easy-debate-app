482381ed224113b37ac7fe72ade71f238b4ba751
DN0N <hanawa.0616@gmail.com>
2026-08-11 19:15:07 +0900

    prisma関連の指摘取り込み

--- backend/prisma/migrations/20260811095013/migration.sql
+++ backend/prisma/migrations/20260811095013/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE `users` (
+CREATE TABLE `user` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `user_name` VARCHAR(20) NOT NULL,
     `password_hash` VARCHAR(255) NOT NULL,
@@ -10,14 +10,14 @@ CREATE TABLE `users` (
     `deleted_at` DATETIME(3) NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    UNIQUE INDEX `users_user_name_delete_marker_key`(`user_name`, `delete_marker`),
+    UNIQUE INDEX `user_user_name_delete_marker_key`(`user_name`, `delete_marker`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `login_session` (
     `session_id_hash` CHAR(64) NOT NULL,
-    `users_id` INTEGER NOT NULL,
+    `user_id` INTEGER NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `last_login_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
@@ -27,11 +27,11 @@ CREATE TABLE `login_session` (
 -- CreateTable
 CREATE TABLE `battle_history` (
     `id` CHAR(36) NOT NULL,
-    `users_id` INTEGER NOT NULL,
+    `user_id` INTEGER NOT NULL,
     `winner_flag` BOOLEAN NOT NULL,
     `matched_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    PRIMARY KEY (`id`, `users_id`)
+    PRIMARY KEY (`id`, `user_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
@@ -78,24 +78,19 @@ CREATE TABLE `moral_violation_category` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `login_session` ADD CONSTRAINT `login_session_users_id_fkey` FOREIGN KEY (`users_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `login_session` ADD CONSTRAINT `login_session_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `battle_history` ADD CONSTRAINT `battle_history_users_id_fkey` FOREIGN KEY (`users_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `battle_history` ADD CONSTRAINT `battle_history_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `rate_updown_history` ADD CONSTRAINT `rate_updown_history_battle_history_id_user_id_fkey` FOREIGN KEY (`battle_history_id`, `user_id`) REFERENCES `battle_history`(`id`, `users_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `rate_updown_history` ADD CONSTRAINT `rate_updown_history_battle_history_id_user_id_fkey` FOREIGN KEY (`battle_history_id`, `user_id`) REFERENCES `battle_history`(`id`, `user_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `moral_updown_history` ADD CONSTRAINT `moral_updown_history_battle_history_id_user_id_fkey` FOREIGN KEY (`battle_history_id`, `user_id`) REFERENCES `battle_history`(`id`, `users_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `moral_updown_history` ADD CONSTRAINT `moral_updown_history_battle_history_id_user_id_fkey` FOREIGN KEY (`battle_history_id`, `user_id`) REFERENCES `battle_history`(`id`, `user_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `moral_violation_history` ADD CONSTRAINT `moral_violation_history_battle_history_id_user_id_fkey` FOREIGN KEY (`battle_history_id`, `user_id`) REFERENCES `battle_history`(`id`, `users_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `moral_violation_history` ADD CONSTRAINT `moral_violation_history_battle_history_id_user_id_fkey` FOREIGN KEY (`battle_history_id`, `user_id`) REFERENCES `battle_history`(`id`, `user_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `moral_violation_history` ADD CONSTRAINT `moral_violation_history_moral_violation_category_id_fkey` FOREIGN KEY (`moral_violation_category_id`) REFERENCES `moral_violation_category`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- usersテーブル「レート」「モラルスコア」のCHECK制約を記載
-ALTER TABLE `users`
-    ADD CONSTRAINT `chk_users_rate` CHECK (`rate` BETWEEN 0 AND 9999),
-    ADD CONSTRAINT `chk_users_moral_score` CHECK (`moral_score` BETWEEN 0 AND 9999);
