684bb6ce3a5efc01469b526f3875722dbad4d3b1
moririn-60 <okoume.daisuki@gmail.com>
2026-08-25 20:11:35 +0900

    再指摘内容修正

--- frontend/src/mocks/socket/endpoint/debate_chatSend.tsx
+++ frontend/src/mocks/socket/endpoint/debate_chatSend.tsx
@@ -36,50 +36,50 @@ const debateChatSend: ScenarioHandler = (server) => {
   };
   server.emit('debate:chatReceive', payload, 1000);
 
-  // server.emit('judge:result', undefined, 3000);
+  // 10ターン終了で勝敗判定へ進むケースを試したい場合は以下のコメントを削除
 
-  const now = Date.now();
-  const judgeResult = {
-    judgeDisplayStartAt: new Date(now + 15 * 1000).toISOString(),
-    judgeConfirmDeadline: new Date(now + 17 * 1000).toISOString(),
-    judgeReason: '具体的な根拠を提示し、相手の主張に的確に反論できていたため。',
-    users: [
-      {
-        userId: 1,
-        isWinner: true,
-        updatedRate: 1520,
-        rateUpDown: 20,
-      },
-      {
-        userId: 2,
-        isWinner: false,
-        updatedRate: 1480,
-        rateUpDown: -20,
-      },
-    ],
-    violation: {
-      isMoralViolationOfBattle: false,
-      is2NoChat: false,
-      isLeave: false,
-    },
-    isRematch: true,
-    thanks: [
-      {
-        fixedThanksId: 1,
-        fixedThanksMsg: 'ありがとうございました！',
-      },
-      {
-        fixedThanksId: 2,
-        fixedThanksMsg: '楽しかったです！',
-      },
-      {
-        fixedThanksId: 3,
-        fixedThanksMsg: '再対戦しませんか？',
-      },
-    ],
-  };
+  //   const now = Date.now();
+  //   const judgeResult = {
+  //     judgeDisplayStartAt: new Date(now + 15 * 1000).toISOString(),
+  //     judgeConfirmDeadline: new Date(now + 17 * 1000).toISOString(),
+  //     judgeReason: '具体的な根拠を提示し、相手の主張に的確に反論できていたため。',
+  //     users: [
+  //       {
+  //         userId: 1,
+  //         isWinner: true,
+  //         updatedRate: 1520,
+  //         rateUpDown: 20,
+  //       },
+  //       {
+  //         userId: 2,
+  //         isWinner: false,
+  //         updatedRate: 1480,
+  //         rateUpDown: -20,
+  //       },
+  //     ],
+  //     violation: {
+  //       isMoralViolationOfBattle: false,
+  //       is2NoChat: false,
+  //       isLeave: false,
+  //     },
+  //     isRematch: true,
+  //     thanks: [
+  //       {
+  //         fixedThanksId: 1,
+  //         fixedThanksMsg: 'ありがとうございました！',
+  //       },
+  //       {
+  //         fixedThanksId: 2,
+  //         fixedThanksMsg: '楽しかったです！',
+  //       },
+  //       {
+  //         fixedThanksId: 3,
+  //         fixedThanksMsg: '再対戦しませんか？',
+  //       },
+  //     ],
+  //   };
 
-  server.emit('judge:result', judgeResult, 500);
+  //   server.emit('judge:result', judgeResult, 500);
 };
 
 export { debateChatSend };
